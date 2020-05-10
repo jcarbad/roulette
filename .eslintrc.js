467e4d09356bfeb1b01b35a0713b8173536537d5
Armando Carballo <joan.carballo@homeadvisor.com>
2020-05-09 21:43:01 -0600

    Avoids warnings from breaking the build

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,9 @@ module.exports = {
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    options: {
+      emitWarning: true,
+    },
   }
 };
